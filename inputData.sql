931f15830c902f9d49dd5a7e14e5fb177aa5e5b3
ColinChenoweth <chenoweth.colin@gmail.com>
2022-04-16 20:25:57 -0400

    Changed file locations

--- inputData.sql
+++ inputData.sql
@@ -1,46 +1,46 @@
-LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\VS\\VS_School\\341\\airport.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\GitHub\\AirportManagementSystem\\FakeData\\airport.csv'
 INTO TABLE airport
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\VS\\VS_School\\341\\schedule.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\GitHub\\AirportManagementSystem\\FakeData\\schedule.csv'
 INTO TABLE schedule
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\VS\\VS_School\\341\\plane.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\GitHub\\AirportManagementSystem\\FakeData\\plane.csv'
 INTO TABLE plane
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\VS\\VS_School\\341\\customer.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\GitHub\\AirportManagementSystem\\FakeData\\customer.csv'
 INTO TABLE customer
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\VS\\VS_School\\341\\seat.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\GitHub\\AirportManagementSystem\\FakeData\\seat.csv'
 INTO TABLE seat
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\VS\\VS_School\\341\\ticket.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\GitHub\\AirportManagementSystem\\FakeData\\ticket.csv'
 INTO TABLE ticket
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\VS\\VS_School\\341\\airline_company.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\chenb\\Documents\\GitHub\\AirportManagementSystem\\FakeData\\airline_company.csv'
 INTO TABLE airline_company
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
